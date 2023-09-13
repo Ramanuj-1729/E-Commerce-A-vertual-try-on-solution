96673db826e0eeb143a3b7740f6c1e3e7dcb6e26
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2023-09-13 20:25:27 +0530

    changel auth to user controller

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,5 +1,5 @@
-const registerController = require('../controllers/authControllers/registerController');
-const loginController = require('../controllers/authControllers/loginController');
+const registerController = require('../controllers/userControllers/registerController');
+const loginController = require('../controllers/userControllers/loginController');
 const productController = require('../controllers/productControllers/productController');
 const categoryController = require('../controllers/productControllers/categoryController');
 const orderController = require('../controllers/orderControllers/orderController');
