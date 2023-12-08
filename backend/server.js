d8133585edb84f8c05b6ddc1178682b8ee211f41
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2023-12-08 15:18:00 +0530

    Update CORS origin in server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ db.once('open', () => console.log('DB connected ...'));
 
 global.appRoot = path.resolve(__dirname);
 const corsOptions = {
-    origin: 'https://ecocart.vercel.app/',
+    origin: 'https://ecocart.vercel.app',
     credentials: true,
     optionSuccessStatus: 200
 }
